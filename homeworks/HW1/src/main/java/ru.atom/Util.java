1c3dffe75aab380b68b6daa9add657b6b5de3009
Firestorm01X2 <firestorm01x2@gmail.com>
2019-02-23 18:37:20 +0300

    Добавлен main метод в лабу 1

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -7,7 +7,13 @@ package ru.atom;
  */
 public class Util {
 
+    public static void main(String[] args) {
+        System.out.println(getHelloWorld());
+    }
 
+    public static String getHelloWorld() {
+        return "Hello, World!";
+    }
 
     /**
      * Returns the greatest of {@code int} values.
