96b6ac6ee0567cc2d9fb6c0e2d88282ab38b6585
Firestorm01X2 <firestorm01x2@gmail.com>
2019-03-03 21:24:04 +0300

    Initial commit

---
+++ homeworks/HW2/src/main/java/ru/example/BullsnCows.java
@@ -0,0 +1,11 @@
+package ru.example;
+
+public class BullsnCows {
+    public static void main(String[] args) {
+        System.out.println("BullsnCows");
+    }
+
+    public static int forTest() {
+        return 9000;
+    }
+}
