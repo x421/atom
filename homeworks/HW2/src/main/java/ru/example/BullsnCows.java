77e57933742fcf867ab2961d26b1d31e4ec74061
Firestorm01X2 <firestorm01x2@gmail.com>
2019-03-04 17:10:09 +0300

    Подключен логгер

--- homeworks/HW2/src/main/java/ru/example/BullsnCows.java
+++ homeworks/HW2/src/main/java/ru/example/BullsnCows.java
@@ -1,7 +1,12 @@
 package ru.example;
 
+import org.slf4j.LoggerFactory;
+
 public class BullsnCows {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(BullsnCows.class);
+
     public static void main(String[] args) {
+        log.info("Логгер работает!");
         System.out.println("BullsnCows");
     }
 
