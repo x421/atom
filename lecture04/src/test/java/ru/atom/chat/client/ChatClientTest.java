d97cc1f63f652a963f2b4e439552b9546d3936ed
Firestorm01X2 <firestorm01x2@gmail.com>
2019-03-14 21:48:06 +0300

    Добавлен запуск Spring при старте тестов

--- lecture04/src/test/java/ru/atom/chat/client/ChatClientTest.java
+++ lecture04/src/test/java/ru/atom/chat/client/ChatClientTest.java
@@ -4,10 +4,16 @@ import okhttp3.Response;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.atom.chat.client.ChatClient;
+import ru.atom.chat.server.ChatApplication;
 
 import java.io.IOException;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={ChatApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class ChatClientTest {
     private static String MY_NAME_IN_CHAT = "I_AM_STUPID";
     private static String MY_MESSAGE_TO_CHAT = "SOMEONE_KILL_ME";
