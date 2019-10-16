8b491f3a607aacee9c0b318170e810b9ca996828
x421 <mr.dubstebmix@gmail.com>
2019-10-16 21:05:24 +0300

    CodeStyle fixes

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -26,8 +26,8 @@ public class Util {
      */
     public static int max(int[] values) {
         int max = values[0];
-        for(int i = 0; i < values.length; i++)
-            if(max < values[i])
+        for (int i = 0; i < values.length; i++)
+            if (max < values[i])
                 max = values[i];
         return max;
     }
@@ -40,8 +40,8 @@ public class Util {
      */
     public static long sum(int[] values) {
         long max = 0;
-        for(int i = 0; i < values.length; i++)
-            max+=values[i];
+        for (int i = 0; i < values.length; i++)
+            max += values[i];
         return max;
     }
 
